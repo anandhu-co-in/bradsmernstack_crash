4d323ef4c0daf78a3645861b2bb5fbd07918e160
codeandcoins <mail.anandhu@gmail.com>
2022-03-01 08:40:49 +0530

    Completed mongodb integration

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const { errorHandler } = require('./middlewear/errorMiddleWear');
 const dotenv = require('dotenv').config()
+const connectDB = require('./config/db');
+
+
+connectDB()
+
 
 const port = process.env.PORT
-
 const app = express()
+
 
 //Below to are required to parse, body from req
 app.use(express.json())
