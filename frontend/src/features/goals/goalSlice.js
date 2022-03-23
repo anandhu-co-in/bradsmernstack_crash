4770b802b88ea261bb7b32b554ebed72d35c859a
codeandcoins <mail.anandhu@gmail.com>
2022-03-23 07:53:37 +0530

    missed to add the goalservice and goalslice files into tracking before last push

---
+++ frontend/src/features/goals/goalSlice.js
@@ -0,0 +1,23 @@
+import {createSlice,createAsyncThunk} from '@reduxjs/toolkit'
+
+import goalService from './goalService'
+
+const initialState = {
+    goals : [],
+    isError:false,
+    isSuccess:false,
+    isLoading:false,
+    message:''
+}
+
+export const goalSlice= createSlice({
+    name:'goals',
+    initialState,
+    reducers :{
+        reset : (state) => initialState
+    },
+})
+
+
+export const {reset} = goalSlice.actions
+export default goalSlice.reducer
